a15f0e8f8450f9e4bbf4e30b9b718344a46d1704
Dimitar Bosnev <dimitar.bosnew@gmail.com>
2025-10-19 17:53:48 +0300

    some updates

--- main.cpp
+++ main.cpp
@@ -5,35 +5,28 @@ float windowWidth = 1024, windowHeight = 768;
 float modelPageOneZRotation = 0;
 uint32_t ACTIVE_RENDERER_INDEX = 0;
 
-const uint16_t FONT_ID_TITLE_56 = 1;
-const uint16_t FONT_ID_BODY_24 = 2;
-const uint16_t FONT_ID_BODY_36 = 3;
-const uint16_t FONT_ID_TITLE_36 = 4;
+const uint16_t FONT_ID_BARTLE = 0;
+const uint16_t FONT_ID_BOGLE = 1;
+
+
+#define DEFAULT_SPACING 16
+#define DEFAULT_CORNER 10
 
 const Clay_Color COLOR_LIGHT = (Clay_Color) {244, 235, 230, 255};
 const Clay_Color COLOR_LIGHT_HOVER = (Clay_Color) {224, 215, 210, 255};
-const Clay_Color COLOR_RED = (Clay_Color) {168, 66, 28, 255};
+const Clay_Color COLOR_RED = (Clay_Color) {209, 52, 52, 255};
 const Clay_Color COLOR_RED_HOVER = (Clay_Color) {148, 46, 8, 255};
 const Clay_Color COLOR_ORANGE = (Clay_Color) {225, 138, 50, 255};
-//const Clay_Color COLOR_BLUE = (Clay_Color) {111, 173, 162, 255};
+const Clay_Color COLOR_GREEN = (Clay_Color) {56, 186, 47, 255};
+const Clay_Color COLOR_BLUE = (Clay_Color) {52, 112, 168, 255};
+const Clay_Color COLOR_BROWN = (Clay_Color) {61, 26, 5, 255};
 
-// Colors for top stripe
-const Clay_Color COLOR_TOP_BORDER_1 = (Clay_Color) {168, 66, 28, 255};
-const Clay_Color COLOR_TOP_BORDER_2 = (Clay_Color) {223, 110, 44, 255};
-const Clay_Color COLOR_TOP_BORDER_3 = (Clay_Color) {225, 138, 50, 255};
-const Clay_Color COLOR_TOP_BORDER_4 = (Clay_Color) {236, 189, 80, 255};
-const Clay_Color COLOR_TOP_BORDER_5 = (Clay_Color) {240, 213, 137, 255};
+const Clay_LayoutConfig defaultLayoutConfig = (Clay_LayoutConfig) { .layoutDirection = CLAY_TOP_TO_BOTTOM, .sizing = { CLAY_SIZING_GROW(0), CLAY_SIZING_GROW(0) }, .childGap = DEFAULT_SPACING, .padding = CLAY_PADDING_ALL(DEFAULT_SPACING) };
 
-const Clay_Color COLOR_BLOB_BORDER_1 = (Clay_Color) {168, 66, 28, 255};
-const Clay_Color COLOR_BLOB_BORDER_2 = (Clay_Color) {203, 100, 44, 255};
-const Clay_Color COLOR_BLOB_BORDER_3 = (Clay_Color) {225, 138, 50, 255};
-const Clay_Color COLOR_BLOB_BORDER_4 = (Clay_Color) {236, 159, 70, 255};
-const Clay_Color COLOR_BLOB_BORDER_5 = (Clay_Color) {240, 189, 100, 255};
-
-#define RAYLIB_VECTOR2_TO_CLAY_VECTOR2(vector) (Clay_Vector2) { .x = (vector).x, .y = (vector).y }
-Clay_TextElementConfig titleTextConfig = (Clay_TextElementConfig) { .fontId = 2, .fontSize = 32, .textColor = {61, 26, 5, 255}, .wrapMode = CLAY_TEXT_WRAP_NONE };
-Clay_TextElementConfig headerTextConfig = (Clay_TextElementConfig) { .fontId = 2, .fontSize = 24, .textColor = {61, 26, 5, 255} };
-Clay_TextElementConfig blobTextConfig = (Clay_TextElementConfig) { .fontId = 2, .fontSize = 30, .textColor = {244, 235, 230, 255} };
+Clay_TextElementConfig titleTextConfig = (Clay_TextElementConfig) { .fontId = FONT_ID_BARTLE, .fontSize = 32, .textColor = COLOR_RED };
+Clay_TextElementConfig headerTextConfig = (Clay_TextElementConfig) { .fontId = FONT_ID_BARTLE, .fontSize = 24, .textColor = COLOR_BROWN };
+Clay_TextElementConfig blobTextConfig = (Clay_TextElementConfig) { .fontId = FONT_ID_BARTLE, .fontSize = 30, .textColor = COLOR_LIGHT };
+Clay_TextElementConfig sideBarTextConfig = (Clay_TextElementConfig) { .fontId = FONT_ID_BOGLE, .fontSize = 36, .textColor = COLOR_RED };
 
 typedef struct {
     void* memory;
@@ -42,11 +35,21 @@ typedef struct {
 
 Arena frameArena = {};
 
-typedef struct d {
+typedef struct {
     Clay_String link;
     bool cursorPointer;
     bool disablePointerEvents;
 } CustomHTMLData;
+
+typedef struct {
+    float mousePositionX, mousePositionY;
+    float deltaTime;
+    bool isTouchDown, isMouseDown;
+    bool isMousePressed, isMouseReleased;
+} InputState;
+
+InputState input;
+
 
 CustomHTMLData* FrameAllocateCustomData(CustomHTMLData data) {
     CustomHTMLData *customData = (CustomHTMLData *)((char*)frameArena.memory + frameArena.offset);
@@ -72,8 +75,6 @@ Clay_Color ColorLerp(Clay_Color a, Clay_Color b, float amount) {
     };
 }
 
-Clay_String LOREM_IPSUM_TEXT = CLAY_STRING("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
-
 void HandleRendererButtonInteraction(Clay_ElementId elementId, Clay_PointerData pointerInfo, intptr_t userData) {
     if (pointerInfo.state == CLAY_POINTER_DATA_PRESSED_THIS_FRAME) {
         ACTIVE_RENDERER_INDEX = (uint32_t)userData;
@@ -89,7 +90,7 @@ void RendererButtonActive(Clay_String text) {
         .cornerRadius = CLAY_CORNER_RADIUS(10),
         .userData = FrameAllocateCustomData((CustomHTMLData) { .disablePointerEvents = true, .cursorPointer = true })
     }) {
-        CLAY_TEXT(text, CLAY_TEXT_CONFIG({ .fontSize = 28, .fontId = FONT_ID_BODY_36, .textColor = COLOR_LIGHT }));
+        CLAY_TEXT(text, CLAY_TEXT_CONFIG({ .fontSize = 28, .fontId = FONT_ID_BARTLE, .textColor = COLOR_LIGHT }));
     }
 }
 
@@ -102,7 +103,7 @@ void RendererButtonInactive(Clay_String text, size_t rendererIndex) {
         .userData = FrameAllocateCustomData((CustomHTMLData) { .disablePointerEvents = true, .cursorPointer = true })
     }) {
         Clay_OnHover(HandleRendererButtonInteraction, rendererIndex);
-        CLAY_TEXT(text, CLAY_TEXT_CONFIG({ .fontSize = 28, .fontId = FONT_ID_BODY_36, .textColor = COLOR_RED }));
+        CLAY_TEXT(text, CLAY_TEXT_CONFIG({ .fontSize = 28, .fontId = FONT_ID_BARTLE, .textColor = COLOR_RED }));
     }
 }
 
@@ -115,73 +116,86 @@ typedef struct
 
 typedef CLAY_PACKED_ENUM{
     PAGE_MAIN,
-    PAGE_CHAMPIONS,
-    PAGE_CHAMPION_INFO,
-    PAGE_CHAMPION_DRAFT,
+    PAGE_CHARACTERS,
+    PAGE_CHARACTER_INFO,
+    PAGE_CHARACTER_DRAFT,
     PAGE_IN_GAME
 }Page;
 Page current_page;
 
 ScrollbarData scrollbarData = (ScrollbarData) {};
 float animationLerpValue = -1.0f;
-
-void MainLayout(){
-CLAY(CLAY_ID("OuterContainer"), { .layout = { .layoutDirection = CLAY_LEFT_TO_RIGHT, .sizing = { CLAY_SIZING_GROW(0), CLAY_SIZING_GROW(0) } }, .backgroundColor = COLOR_LIGHT }) {
-        CLAY(CLAY_ID("Header"), { .layout = { .layoutDirection = CLAY_TOP_TO_BOTTOM, .sizing = { CLAY_SIZING_FIXED(100), CLAY_SIZING_GROW(0) }, .childAlignment = { CLAY_ALIGN_X_LEFT, CLAY_ALIGN_Y_CENTER }, .childGap = 16, .padding = { 32, 32, 16 } } }) {
-            CLAY_TEXT(CLAY_STRING("League of the Ancients"), &titleTextConfig);
-            CLAY(CLAY_ID("Spacer_TOP"), { .layout = { .sizing = { .height = CLAY_SIZING_GROW(0) } } }) {}
-            CLAY_AUTO_ID({
-                .layout = { .padding = {16, 16, 6, 6} },
-                .backgroundColor = Clay_Hovered() ? COLOR_LIGHT_HOVER : COLOR_LIGHT,
-                .border = { .width = {2, 2, 2, 2}, .color = COLOR_RED },
-                .cornerRadius = CLAY_CORNER_RADIUS(10),
-                .userData = FrameAllocateCustomData((CustomHTMLData) { .link = CLAY_STRING("https://discord.gg/b4FTWkxdvT") }),
-            }) {
-                CLAY_TEXT(CLAY_STRING("Discord"), CLAY_TEXT_CONFIG({
-                    .userData = FrameAllocateCustomData((CustomHTMLData) { .disablePointerEvents = true }),
-                    .fontId = FONT_ID_BODY_24, .fontSize = 24, .textColor = {61, 26, 5, 255} }));
-            }
-            CLAY_AUTO_ID({
-                .layout = { .padding = {16, 16, 6, 6} },
-                .backgroundColor = Clay_Hovered() ? COLOR_LIGHT_HOVER : COLOR_LIGHT,
-                .border = { .width = {2, 2, 2, 2}, .color = COLOR_RED },
-                .cornerRadius = CLAY_CORNER_RADIUS(10),
-                .userData = FrameAllocateCustomData((CustomHTMLData) { .link = CLAY_STRING("https://discord.gg/b4FTWkxdvT") }),
-            }) {
-                CLAY_TEXT(CLAY_STRING("Discord"), CLAY_TEXT_CONFIG({
-                    .userData = FrameAllocateCustomData((CustomHTMLData) { .disablePointerEvents = true }),
-                    .fontId = FONT_ID_BODY_24, .fontSize = 24, .textColor = {61, 26, 5, 255} }));
-            }
-            CLAY_AUTO_ID({
-                .layout = { .padding = {16, 16, 6, 6} },
-                .backgroundColor = Clay_Hovered() ? COLOR_LIGHT_HOVER : COLOR_LIGHT,
-                .border = { .width = {2, 2, 2, 2}, .color = COLOR_RED },
-                .cornerRadius = CLAY_CORNER_RADIUS(10),
-                .userData = FrameAllocateCustomData((CustomHTMLData) { .link = CLAY_STRING("https://github.com/nicbarker/clay") }),
-            }) {
-                CLAY_TEXT(CLAY_STRING("Github"), CLAY_TEXT_CONFIG({
-                    .userData = FrameAllocateCustomData((CustomHTMLData) { .disablePointerEvents = true }),
-                    .fontId = FONT_ID_BODY_24, .fontSize = 24, .textColor = {61, 26, 5, 255} }));
-            }
+void SideBar(){
+    sideBarTextConfig.userData = FrameAllocateCustomData((CustomHTMLData) { .disablePointerEvents = true });
+    CLAY(CLAY_ID("SideBar"), { .layout = { .layoutDirection = CLAY_TOP_TO_BOTTOM, .sizing = { CLAY_SIZING_FIT(0), CLAY_SIZING_GROW(0) }, .childAlignment = { CLAY_ALIGN_X_LEFT, CLAY_ALIGN_Y_CENTER }, .childGap = DEFAULT_SPACING } }) {
+        CLAY(CLAY_ID("Spacer_TOP"), { .layout = { .sizing = { .height = CLAY_SIZING_GROW(0) } } }) {}
+        CLAY_AUTO_ID({
+            .layout = { .padding = {16, 16, 6, 6} },
+            .backgroundColor = Clay_Hovered() || current_page == PAGE_MAIN ? COLOR_LIGHT_HOVER : COLOR_LIGHT,
+            .border = { .width = {2, 2, 2, 2}, .color = COLOR_RED },
+            .cornerRadius = CLAY_CORNER_RADIUS(DEFAULT_CORNER),
+        }) {
+            if(Clay_Hovered() && input.isMouseReleased) current_page = PAGE_MAIN;
+            CLAY_TEXT(CLAY_STRING("Home"), &sideBarTextConfig);
+        }
+        CLAY_AUTO_ID({
+            .layout = { .padding = {16, 16, 6, 6} },
+            .backgroundColor = Clay_Hovered() || current_page == PAGE_CHARACTERS ? COLOR_LIGHT_HOVER : COLOR_LIGHT,
+            .border = { .width = {2, 2, 2, 2}, .color = COLOR_RED },
+            .cornerRadius = CLAY_CORNER_RADIUS(DEFAULT_CORNER),
+        }) {
+            if(Clay_Hovered() && input.isMouseReleased) current_page = PAGE_CHARACTERS;
+            CLAY_TEXT(CLAY_STRING("Characters"), &sideBarTextConfig);
+        }
+        CLAY_AUTO_ID({
+            .layout = { .padding = {16, 16, 6, 6} },
+            .backgroundColor = Clay_Hovered() || current_page == PAGE_CHARACTER_DRAFT? COLOR_LIGHT_HOVER : COLOR_LIGHT,
+            .border = { .width = {2, 2, 2, 2}, .color = COLOR_RED },
+            .cornerRadius = CLAY_CORNER_RADIUS(DEFAULT_CORNER)
+        }) {
+            //if(Clay_Hovered()) current_page = PAGE_CHARACTER_DRAFT;
+            CLAY_TEXT(CLAY_STRING("Battle"), &sideBarTextConfig);
+        }
             CLAY(CLAY_ID("Spacer_BOT"), { .layout = { .sizing = { .height = CLAY_SIZING_GROW(0) } } }) {}
+    }
+}
+void MainLayout(){
+CLAY(CLAY_ID("OuterContainer"), { .layout = { .layoutDirection = CLAY_TOP_TO_BOTTOM, .sizing = { CLAY_SIZING_GROW(0), CLAY_SIZING_GROW(0) }, .padding = CLAY_PADDING_ALL(DEFAULT_SPACING), .childGap = DEFAULT_SPACING }, .backgroundColor = COLOR_LIGHT }) {
+        CLAY_TEXT(CLAY_STRING("League of the Ancients"), &titleTextConfig);
+        CLAY(CLAY_ID("PageContainer"), { .layout = { .layoutDirection = CLAY_LEFT_TO_RIGHT, .sizing = { CLAY_SIZING_GROW(0), CLAY_SIZING_GROW(0) }, .childGap = DEFAULT_SPACING }, .backgroundColor = COLOR_LIGHT, }){
+            SideBar();
+            CLAY(CLAY_ID("FILLER"), { .backgroundColor = COLOR_RED, .cornerRadius = CLAY_CORNER_RADIUS(DEFAULT_CORNER), .layout = defaultLayoutConfig});
+            CLAY(CLAY_ID("FILLER2"), { .backgroundColor = COLOR_ORANGE, .cornerRadius = CLAY_CORNER_RADIUS(DEFAULT_CORNER), .layout= defaultLayoutConfig});
         }
     }
 }
 
 void ChampionsLayout(){
-
+CLAY(CLAY_ID("OuterContainer"), { .layout = { .layoutDirection = CLAY_TOP_TO_BOTTOM, .sizing = { CLAY_SIZING_GROW(0), CLAY_SIZING_GROW(0) }, .padding = CLAY_PADDING_ALL(DEFAULT_SPACING), .childGap = DEFAULT_SPACING }, .backgroundColor = COLOR_LIGHT }) {
+        CLAY_TEXT(CLAY_STRING("League of the Ancients"), &titleTextConfig);
+        CLAY(CLAY_ID("PageContainer"), { .layout = { .layoutDirection = CLAY_LEFT_TO_RIGHT, .sizing = { CLAY_SIZING_GROW(0), CLAY_SIZING_GROW(0) }, .childGap = DEFAULT_SPACING }, .backgroundColor = COLOR_LIGHT, }){
+            SideBar();
+            CLAY(CLAY_ID("FILLER"), { .backgroundColor = COLOR_RED, .cornerRadius = CLAY_CORNER_RADIUS(DEFAULT_CORNER), .layout = defaultLayoutConfig});
+            CLAY(CLAY_ID("FILLER2"), { .backgroundColor = COLOR_ORANGE, .cornerRadius = CLAY_CORNER_RADIUS(DEFAULT_CORNER), .layout = defaultLayoutConfig});
+        }
+    }
 }
 
 void ChampionInfoLayout(){
-    
+    CLAY(CLAY_ID("OuterContainer"), { .layout = { .layoutDirection = CLAY_TOP_TO_BOTTOM, .sizing = { CLAY_SIZING_GROW(0), CLAY_SIZING_GROW(0) }, .padding = CLAY_PADDING_ALL(DEFAULT_SPACING), .childGap = DEFAULT_SPACING }, .backgroundColor = COLOR_LIGHT }) {
+        CLAY_TEXT(CLAY_STRING("League of the Ancients"), &titleTextConfig);
+        CLAY(CLAY_ID("PageContainer"), { .layout = { .layoutDirection = CLAY_LEFT_TO_RIGHT, .sizing = { CLAY_SIZING_GROW(0), CLAY_SIZING_GROW(0) }, .childGap = DEFAULT_SPACING }, .backgroundColor = COLOR_LIGHT, }){
+            SideBar();
+            CLAY(CLAY_ID("FILLER"), { .backgroundColor = COLOR_RED, .cornerRadius = CLAY_CORNER_RADIUS(DEFAULT_CORNER), .layout = defaultLayoutConfig});
+            CLAY(CLAY_ID("FILLER2"), { .backgroundColor = COLOR_ORANGE, .cornerRadius = CLAY_CORNER_RADIUS(DEFAULT_CORNER), .layout = defaultLayoutConfig});
+        }
+    }
 }
 
 void ChampionDraftLayout(){
-
 }
 
 void InGameLayout(){
-
 }
 
 
@@ -191,9 +205,9 @@ Clay_RenderCommandArray CreateLayout(bool mobileScreen, float lerpValue) {
     switch (current_page)
     {
         case PAGE_MAIN: MainLayout(); break;
-        case PAGE_CHAMPIONS: ChampionsLayout(); break;
-        case PAGE_CHAMPION_INFO: ChampionInfoLayout(); break;
-        case PAGE_CHAMPION_DRAFT: ChampionDraftLayout(); break;
+        case PAGE_CHARACTERS: ChampionsLayout(); break;
+        case PAGE_CHARACTER_INFO: ChampionInfoLayout(); break;
+        case PAGE_CHARACTER_DRAFT: ChampionDraftLayout(); break;
         case PAGE_IN_GAME: InGameLayout(); break;
     }
     
@@ -277,6 +291,15 @@ CLAY_WASM_EXPORT("UpdateDrawFrame") Clay_RenderCommandArray UpdateDrawFrame(floa
     if (debugModeEnabled) {
         isMobileScreen = windowWidth < 950;
     }
+
+    input.deltaTime = deltaTime;
+    input.isMousePressed = !input.isMouseDown && isMouseDown;
+    input.isMouseReleased = input.isMouseDown && !isMouseDown;
+    input.isMouseDown = isMouseDown;
+    input.isTouchDown = isTouchDown;
+    input.mousePositionX = mousePositionX;
+    input.mousePositionY = mousePositionY;
+
     return CreateLayout(isMobileScreen, animationLerpValue < 0 ? (animationLerpValue + 1) : (1 - animationLerpValue));
     //----------------------------------------------------------------------------------
 }
