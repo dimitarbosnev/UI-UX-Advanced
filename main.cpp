112cbcbc99a88c92b2a4a0edabc4093c7db9ca60
Dimitar Bosnev <dimitar.bosnew@gmail.com>
2025-10-25 13:17:44 +0300

    style changes

--- main.cpp
+++ main.cpp
@@ -682,8 +682,8 @@ void MainLayout(){
                         CLAY_TEXT(CLAY_STRING("Progress: 0/20"), &smallTextConfig);
                     }
                 }
-                CLAY_AUTO_ID({ .layout = { .sizing = { .width = CLAY_SIZING_GROW(0) } }, .border = { .width = {0, 0, 0, 2}, .color = COLOR_LIGHT }});
-                                        CLAY_AUTO_ID({.layout = { .layoutDirection = CLAY_TOP_TO_BOTTOM, .sizing = { .width = CLAY_SIZING_GROW(0) }}}){
+                CLAY_AUTO_ID({ .layout = { .sizing = { .width = CLAY_SIZING_GROW(0) } }, .border = { .width = {0, 0, 0, 2}, .color = COLOR_LIGHT}});
+                CLAY_AUTO_ID({.layout = { .layoutDirection = CLAY_TOP_TO_BOTTOM, .sizing = { .width = CLAY_SIZING_GROW(0) }}}){
                     CLAY_TEXT(CLAY_STRING("Demolition"), &defaultTextConfig);
                     CLAY_AUTO_ID({.layout = { .layoutDirection = CLAY_LEFT_TO_RIGHT, .sizing = { .width = CLAY_SIZING_GROW(0) }, .padding = {DEFAULT_SPACING, 0, 0, 0 }}}){
                        CLAY_TEXT(CLAY_STRING("Destroy 5 towers in one game"), &smallTextConfig); 
@@ -703,7 +703,7 @@ void MainLayout(){
                 CLAY_AUTO_ID({.layout = { .layoutDirection = CLAY_LEFT_TO_RIGHT, .sizing = { .width = CLAY_SIZING_GROW(0) }, .padding = {0,0,0,10}}}){
                     CLAY_TEXT(CLAY_STRING(" Zander Comes to the arena: March 2026!"), &defaultTextConfig);
                 }
-                CLAY_AUTO_ID({.layout = {.sizing = { .width = CLAY_SIZING_GROW(0) }}, .image  = { &zander_image }, . aspectRatio = 16.0f/9.0f});
+                CLAY_AUTO_ID({.layout = {.sizing = { .width = CLAY_SIZING_GROW(0) }}, .image  = { &zander_image }, . aspectRatio = {16.0f/9.0f}});
 
             }
             CLAY_AUTO_ID({ .layout = { .sizing = { .width = CLAY_SIZING_GROW(0) } }, .border = { .width = {0, 0, 0, 2}, .color = COLOR_LIGHT }});
